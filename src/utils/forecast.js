d92e9e3694d4b21f276395fcb7bccccf007bf6aa
vren <virenkathiriya96@gmail.com>
2019-07-12 14:06:37 +0530

    updated weather info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback(' Invalid search for location. Try another Search')
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chace of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chace of rain.' + 'Highest and Lowest temperature attained during day are ' + body.daily.data[0].temperatureHigh + ' degrees and ' + body.daily.data[0].temperatureLow + ' degrees respectively!')
         }
     })
 }
